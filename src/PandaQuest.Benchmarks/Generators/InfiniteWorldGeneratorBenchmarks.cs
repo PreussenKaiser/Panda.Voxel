8b5fbca6b7f387bb0f870a0767ef1f13f25f074f
Karl Lukan <klukan20@gmail.com>
2023-10-01 18:09:06 -0500

    added benchmarking proj

---
+++ src/PandaQuest.Benchmarks/Generators/InfiniteWorldGeneratorBenchmarks.cs
@@ -0,0 +1,31 @@
+﻿using BenchmarkDotNet.Attributes;
+using Microsoft.Xna.Framework;
+using PandaQuest.Generators;
+using PandaQuest.Input;
+using PandaQuest.Input.CameraInput;
+using PandaQuest.Input.Movement;
+
+namespace PandaQuest.Benchmarks.Generators;
+
+public class InfiniteWorldGeneratorBenchmarks
+{
+	private readonly IWorldGenerator worldGenerator;
+	private readonly Player player;
+
+	public InfiniteWorldGeneratorBenchmarks()
+	{
+		var mouseInput = new MouseInput(Vector2.Zero);
+		var camera = new Camera(mouseInput, Vector3.Zero, 0);
+		IMovement movement = new GroundedMovement();
+		var player = new Player(camera, movement);
+
+		this.worldGenerator = new InfiniteWorldGenerator(player);
+		this.player = player;
+	}
+
+	[Benchmark]
+	public void RenderChunks()
+	{
+		this.worldGenerator.Generate();
+	}
+}
---
+++ src/PandaQuest.Benchmarks/Program.cs
@@ -0,0 +1,7 @@
+﻿using BenchmarkDotNet.Running;
+using PandaQuest.Benchmarks.Generators;
+
+BenchmarkRunner.Run<InfiniteWorldGeneratorBenchmarks>();
+
+// For breakpoint.
+_ = 1;
